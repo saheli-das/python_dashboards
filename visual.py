abbadad50db51cfef7ed65c5cf49b8ffdf6595c5
saheli-das <saheli.das95@gmail.com>
2025-02-27 16:31:27 +0530

    Update visual.py

--- visual.py
+++ visual.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
 
 # Function to load data from a public Google Drive link
 def load_data_from_google_drive(public_link):
