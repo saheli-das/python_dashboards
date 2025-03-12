c2e5172c3d3a82d526a38dd501f13a3c21ac964c
saheli-das <saheli.das95@gmail.com>
2025-03-12 13:50:18 +0530

    Update visual.py

--- visual.py
+++ visual.py
@@ -343,19 +343,19 @@ if st.session_state["logged_in"]:
 
                 # Visualization 12: Number of Employees Hired by Year
                 st.header("📊 Number of Employees Hired by Year")
-                df["hire_date"] = pd.to_datetime(df["hire_date"], errors="coerce")
-                df["hire_year"] = df["hire_date"].dt.year
-                result = df.groupby("hire_year").size().reset_index(name="employee_count")
-                result = result.sort_values(by="hire_year")
+                df["hire_date"] = pd.to_datetime(df["hire_date"]).dt.year
+                
+                result = df.groupby("hire_date").size().reset_index(name="employee_count")
+                result = result.sort_values(by="hire_date")
                 plt.figure(figsize=(10, 6))
-                sns.lineplot(x="hire_year", y="employee_count", data=result, marker="o", color="blue")
+                sns.lineplot(x="hire_date", y="employee_count", data=result, marker="o", color="blue")
                 plt.title("Number of Employees Hired by Year", fontsize=16)
                 plt.xlabel("Year", fontsize=14)
                 plt.ylabel("Number of Employees", fontsize=14)
                 plt.xticks(fontsize=12)
                 plt.yticks(fontsize=12)
                 for index, row in result.iterrows():
-                    plt.text(row["hire_year"], row["employee_count"] + 0.1, f"{row['employee_count']:,}" , ha="center", va="bottom", fontsize=12)
+                    plt.text(row["hire_date"], row["employee_count"] + 0.1, f"{row['employee_count']:,}" , ha="center", va="bottom", fontsize=12)
                 plt.grid(False)
                 plt.tight_layout()
                 st.pyplot(plt)
